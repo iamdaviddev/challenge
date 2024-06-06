55e5eda959b57b0292877163548b836b89cabbd1
iamdaviddev <davidpaulodev@gmail.com>
2024-06-06 09:48:48 +0100

    atualization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
             <span className="text-zinc-300 font-light">tuesday 19 Aug 2021</span>
             <span className="text-zinc-300 font-light">Gerson Abreu</span>
             <strong className="text-white">£1,880.90</strong>
-            <button className="bg-green-500 bg-opacity-35 p-2 w-28 text-green-400 rounded-md flex items-center justify-around"><Dot/>Paid </button>
+            <button className="bg-green-500 bg-opacity-35 p-2 w-28 text-green-400 rounded-md flex items-center justify-around"><Dot/>Paid</button>
             <ChevronRight className="text-[#7C5DF9]"/>
           </div> 
           <Card/>
